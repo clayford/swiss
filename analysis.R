1a5d4321dca37d09606c18eef6b3360f1d489dd5
Clay Ford <joe.clayton.ford@gmail.com>
2018-03-14 14:38:35 -0400

    add model and effect plots

--- analysis.R
+++ analysis.R
@@ -2,3 +2,10 @@ data(swiss)
 summary(swiss)
 pairs(swiss)
 
+mod <- lm(Fertility ~ ., data = swiss)
+summary(mod)
+
+library(effects)
+
+plot(Effect("Education", mod))
+plot(Effect("Catholic", mod))
